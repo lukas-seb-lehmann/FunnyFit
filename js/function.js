b5414fa5a158f37e3ac69d69bd6c0fb134f96a23
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2020-06-17 12:02:27 +0200

    first upload

---
+++ js/function.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+	
+	navigation: true,
+	navigationPosition: 'left',
+	
+});
