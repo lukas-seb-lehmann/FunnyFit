89814d6eec0bd8185b56a70ba2569fbf3efe464e
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2020-06-17 22:58:21 +0200

    sidemenu

--- js/function.js
+++ js/function.js
@@ -5,13 +5,19 @@ new fullpage('#fullpage', {
 	autoScrolling:true,
 	
 	
-	navigation: true,
+	navigation: false,
 	navigationPosition: 'left',
 	onLeave: function(origin, destination, direction){
+		
 		var leavingSection = this;
 
+	
+				
+			
+		
+		
 		//after leaving section 2
-		if(origin.index == 0 && direction =='down'){
+	if(origin.index == 0 && direction =='down'){
 		
 			if($(".sec1").hasClass("active")){
 		$(".cta").addClass("opacity");
@@ -23,17 +29,48 @@ new fullpage('#fullpage', {
 		
 		$(".cta").removeClass("opacity");
 		$(".pfeil").removeClass("opacity");
-			
 	}
-		
-			
-
-	
+		if(destination.index == 1){
+		$(".nav2").addClass("activ-nav");
+		}if(origin.index == 1){
+		$(".nav2").removeClass("activ-nav");
 		}
-	
 		
+		if(destination.index == 2){
+		$(".nav3").addClass("activ-nav");
+		}if(origin.index == 2){
+		$(".nav3").removeClass("activ-nav");
+		}
+		
+		if(destination.index == 3){
+		$(".nav4").addClass("activ-nav");
+		}if(origin.index == 3){
+		$(".nav4").removeClass("activ-nav");
+		}
+		
+		if(destination.index == 4){
+		$(".nav5").addClass("activ-nav");
+		}if(origin.index == 4){
+		$(".nav5").removeClass("activ-nav");
+		}
+		
+		if(destination.index == 5){
+		$(".nav6").addClass("activ-nav");
+		}if(origin.index == 5){
+		$(".nav6").removeClass("activ-nav");
+		}
+		
+		if(destination.index == 6){
+		$(".nav7").addClass("activ-nav");
+		}if(origin.index == 6){
+		$(".nav7").removeClass("activ-nav");
+		}
+		
+		
+		},
+	
+	
 });
 
 
-	
 	
